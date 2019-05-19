d27ac0e5bd2204d8cd055d6cbe56f52aaedb9503
Alberto Oshiro <albertooshiro90@gmail.com>
2019-05-19 00:49:40 -0300

    chess class

--- src/main/java/framework/core/Chess.java
+++ src/main/java/framework/core/Chess.java
@@ -1,8 +1,44 @@
 package framework.core;
 
+import java.util.List;
+
 public class Chess {
 
+    private Side side;
+    private Board board;
 
+    public Chess() {
+        board = new Board();
+        side = Side.White;
+    }
 
+    public Board getBoard() {
+        return board;
+    }
+
+    public void move(Move move) {
+        this.board = new Board(this.board, move);
+        this.side = switchSide(side);
+    }
+
+    private Side switchSide(Side side) {
+        if (side.equals(Side.White)) {
+            return Side.Black;
+        }
+        return Side.White;
+    }
+
+    public List<Move> getMoves() {
+        return this.board.getValidMoves(this.side);
+    }
+
+    public boolean gameOver() {
+        return (getMoves().isEmpty());
+    }
+
+    public Side getWinner() {
+        assert (gameOver());
+        return switchSide(this.side);
+    }
 
 }
